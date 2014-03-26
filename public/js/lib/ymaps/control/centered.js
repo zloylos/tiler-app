c6015dfbb84fad71917853bbc989e1387b53470d
Denis Hananein <i@zloylos.me>
2014-03-26 21:02:05 +0400

    перенес логику позиционирования в макет

--- public/js/lib/ymaps/control/centered.js
+++ public/js/lib/ymaps/control/centered.js
@@ -8,54 +8,45 @@ modules.define('ymaps-control-centered', [
             '<div class="container-fluid" style="left:{{ options.position.left }}px;top:{{ options.position.top }}px;">',
                 '{% include options.contentBodyLayout %}',
             '</div>'
-        ].join(''));
+        ].join(''), {
+            build: function () {
+                ContentLayout.superclass.build.call(this);
+                this._setupListeners();
+                this._setPosition();
+            },
+
+            clear: function () {
+                this._clearListeners();
+                ContentLayout.superclass.clear.call(this);
+            },
+
+            _setupListeners: function () {
+                var control = this.getData().control;
+                this._mapListener = control.getMap().events.group()
+                    .add('sizechange', this._setPosition, this);
+            },
+
+            _clearListeners: function () {
+                this._mapListener.removeAll();
+            },
+
+            _setPosition: function () {
+                var control = this.getData().control,
+                    mapSize = control.getMap().container.getSize(),
+                    parentElement = this.getParentElement();
+
+                control.options.set('position', {
+                    top: mapSize[1] / 2 - parentElement.offsetHeight / 2,
+                    left: mapSize[0] / 2 - parentElement.offsetWidth / 2
+                });
+            }
+        });
 
     var CenteredControl = inherit(BaseControl, {
         __constructor: function () {
             this.__base.apply(this, arguments);
 
             this.options.set('contentLayout', ContentLayout);
-        },
-        _init: function (el) {
-            this.__base.apply(this, arguments);
-
-            this._setPosition();
-            this._attachHandlers();
-        },
-        _destroy: function () {
-            this._detachHandlers();
-
-            this.__base.apply(this, arguments);
-        },
-        /**
-         * Устанавливает контролу опцию "position".
-         * @function
-         * @private
-         * @name CenteredControl._setPosition
-         * @param {Array} size Размер контейнера карты.
-         */
-        _setPosition: function () {
-            var size = this._map.container.getSize();
-
-            this.options.set('position', {
-                top: size[1] / 2 - this._element.offsetHeight / 2,
-                left: size[0] / 2 - this._element.offsetWidth / 2
-            });
-        },
-        _attachHandlers: function () {
-            this._map.container.events
-                .add('sizechange', this._setPosition, this);
-
-            // this.data.events.add('change', this._setPosition, this);
-
-        },
-        _detachHandlers: function () {
-            if(this._map) {
-                this._map.container.events
-                    .remove('sizechange', this._setPosition, this);
-
-                // this.data.events.remove('change', this._setPosition, this);
-            }
         }
     });
 
